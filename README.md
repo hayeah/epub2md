efaa6296fab73d39cf85aa7c8ae9195893713d96
Howard Yeh <hayeah@gmail.com>
2023-05-06 18:12:02 +0800

    support url download

--- README.md
+++ README.md
@@ -1,18 +1,79 @@
+# Epub to Plain Text Converter
 
-convert epub to plain text
+This application converts an EPUB file to plain text format. You can use it as a command line interface (CLI) tool or run it as an HTTP service.
 
-# cli
+## Installation
+
+Install the required dependencies using `pipenv`:
+
+```
+pipenv install
+```
+
+## Usage
+
+### CLI
+
+To convert an EPUB file to plain text using the CLI, run the following command:
+
+```
+pipenv run python3 main.py <input_epub_file>
+```
+
+Example:
 
 ```
 pipenv run python3 main.py alice-in-wonderland.epub
 ```
 
-# http service
+The plain text output will be saved in the same directory as the input EPUB file.
+
+### HTTP Service
+
+To start the HTTP service, run the following command:
 
 ```
 uvicorn main:app --reload
 ```
 
+#### Uploading an EPUB File
+
+To convert an EPUB file by uploading it, send a POST request with the file to the `/upload` endpoint:
+
 ```
-curl -X POST -F "file=@alice-in-wonderland.epub" http://localhost:8000/upload
+curl -X POST -F "file=@<input_epub_file>" http://localhost:8000/upload -o <output_md_file>
+```
+
+Example:
+
+```
+curl -X POST -F "file=@alice-in-wonderland.epub" http://localhost:8000/upload -o alice.md
+```
+
+#### Providing a URL to an EPUB File
+
+To convert an EPUB file by providing its URL, send a POST request with the URL to the `/upload` endpoint:
+
+```
+curl -X POST "http://localhost:8000/upload?url=<url_encoded_epub_url>" -o <output_md_file>
+```
+
+Example:
+
+```
+curl -X POST "http://localhost:8000/upload?url=https%3A%2F%2Fwww.gutenberg.org%2Febooks%2F11.epub.noimages" -o "alice.md"
+```
+
+## License
+
+This project is licensed under the terms of the MIT License.
+
+# production
+
+```
+curl -X POST -F "file=@alice-in-wonderland.epub" https://epub2md.vercel.app/upload
+
+curl -X POST -F "file=@alice-in-wonderland.epub" https://epub2md-hayeah.vercel.app/upload
+
+
 ```
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import html2text
 import argparse
 import os
+import aiohttp
 
 def epub_to_text(epub_file_path: str):
     book = epub.read_epub(epub_file_path, { "ignore_ncx": True })
@@ -22,26 +23,48 @@ def epub_to_text(epub_file_path: str):
 
 from fastapi import FastAPI, UploadFile, HTTPException, Response
 from tempfile import NamedTemporaryFile
+
 app = FastAPI()
 
-@app.post("/upload")
-async def upload_epub(file: UploadFile = None):
-    if file is None:
-        raise HTTPException(status_code=400, detail="No file uploaded")
+async def process_epub(file: NamedTemporaryFile, filename: str):
+    text = epub_to_text(file.name)
 
-    if file.filename.lower().endswith(".epub"):
+    output_file_path = os.path.splitext(filename)[0] + ".md"
+
+    response = Response(content=text, media_type="text/plain")
+    response.headers["Content-Disposition"] = f'attachment; filename="{output_file_path}"'
+    return response
+
+async def download_from_url(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status != 200:
+                raise HTTPException(status_code=400, detail="Unable to download the file")
+
+            filename = url.split("/")[-1]
+
+            temp_file = NamedTemporaryFile(delete=True)
+            temp_file.write(await resp.read())
+            temp_file.seek(0)
+
+            return temp_file, filename
+
+@app.post("/upload")
+async def upload_epub(file: UploadFile = None, url: str = None):
+    if file is None and url is None:
+        raise HTTPException(status_code=400, detail="No file uploaded or URL provided")
+
+    if file is not None:
+        # if not file.filename.lower().endswith(".epub"):
+        #     raise HTTPException(status_code=400, detail="Invalid file")
+    
         with NamedTemporaryFile(delete=True) as temp_file:
             temp_file.write(await file.read())
             temp_file.seek(0)
-            text = epub_to_text(temp_file.name)
-
-            output_file_path = os.path.splitext(file.filename)[0] + '.md'
-
-            response = Response(content=text, media_type="text/plain")
-            response.headers["Content-Disposition"] = f'attachment; filename="{output_file_path}"'
-            return response
-
-    raise HTTPException(status_code=400, detail="Invalid file")
+            return await process_epub(temp_file, file.filename)
+    else:
+        temp_file, filename = await download_from_url(url)
+        return await process_epub(temp_file, filename)
 
 def cli():
     parser = argparse.ArgumentParser(description='Convert EPUB file to plain text.')
